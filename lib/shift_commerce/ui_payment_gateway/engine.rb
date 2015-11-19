84c7f74e97d2b3d4e84137e2cc7cf3a15868673b
Gary Taylor <gary.taylor@hismessages.com>
2015-11-19 19:16:43 +0000

    Added test mode

--- lib/shift_commerce/ui_payment_gateway/engine.rb
+++ lib/shift_commerce/ui_payment_gateway/engine.rb
@@ -2,6 +2,9 @@ module ShiftCommerce
   module UiPaymentGateway
     class Engine < ::Rails::Engine
       isolate_namespace UiPaymentGateway
+      initializer "Setup active merchant" do
+        ActiveMerchant::Billing::Base.mode = :test if ::ShiftCommerce::UiPaymentGateway::Config.instance.test_mode
+      end
     end
   end
 end
